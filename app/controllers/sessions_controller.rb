90a420567d7401c457005de09d0eafd01cd1d256
Melissa Rodriguez <olenellina@gmail.com>
2016-10-19 14:24:08 -0700

    added flash message to pages that logging in/out redirect to, so that user knows if it was successful

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,27 +6,22 @@ class SessionsController < ApplicationController
     # in this case, back to login page
     if ! auth_hash['uid']
       flash[:notice] = "login failed"
-      return redirect_to merchant_index_path
+      return redirect_to :back
     end
 
     @merchant = Merchant.find_by(uid: auth_hash[:uid], provider: 'github').to_i
     if @merchant.nil?
-      # User doesn't match anything in the DB.
-      # Attempt to create a new user.
-      # This is only because we're building a stub app, should redirect to new user creation
+      # If merchant doesn't match anything in the DB, attempt to create a new user.
       @merchant = Merchant.build_from_github(auth_hash)
       flash[:notice] = "Unable to save this Merchant"
-      # return render :creation_failure unless @user.save
+
       return redirect_to merchant_index_path unless @merchant.save
 
-      # Could add an else here that refreshes/updates the information from Github
     end
 
     # Save the user ID in the session (**not the :uid from GitHub**)
-    # To-do: something is wrong with the merchant id, so creating a stub for now
     session[:user_id] = 4#@merchant.id
 
-    # redirect_to sessions_path
     flash[:notice] = "successfully logged in"
     redirect_to merchant_index_path
   end
@@ -38,6 +33,6 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
 
     flash[:notice] = "you have successfully logged out"
-    redirect_to merchant_index_path
+    redirect_to root_path
   end
 end
