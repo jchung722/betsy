84956f2b38d38e6a430f81e5f996e8ed0b52a973
Emily Abrash <emabrash@gmail.com>
2016-10-24 15:25:32 -0700

    Added orders controller tests (for destroy action)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,14 +12,14 @@ class OrdersController < ApplicationController
   end
 
   def destroy
-    order = Order.find_by(id: session[:order])
-
-    if order
-      order.destroy
+    begin
+      @order = Order.find(session[:order])
+      @order.destroy
+      session[:order] = nil
+      redirect_to carts_index_path
+    rescue ActiveRecord::RecordNotFound
+      render :file => 'public/404.html', :status => :not_found
     end
-
-    session[:order] = nil
-    redirect_to carts_index_path
   end
 
   def update
