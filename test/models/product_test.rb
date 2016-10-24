ec7945c8008446ddee7a522d9e38325b6299ee28
Jessica <jchung722@gmail.com>
2016-10-24 14:59:45 -0700

    review model tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "Product with minimal information must be valid" do
+    assert products(:goods).valid?
+  end
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "Rating must be present to be valid review." do
+    assert reviews(:best).valid?
+    assert_not reviews(:no_rating).valid?
+  end
+
+  test "Rating must be an integer between 1 and 5." do
+    assert_not reviews(:no_integer).valid?
+    assert_not reviews(:low_rating).valid?
+    assert_not reviews(:high_rating).valid?
+  end
+
+  test "Review must belong to a product" do
+    assert_not reviews(:no_product).valid?
+  end
+
 end
