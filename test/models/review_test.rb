75d61ef68709b59b6adfbf487fff5b0127e67868
Jessica <jchung722@gmail.com>
2016-10-25 14:23:20 -0700

    finished refactoring review model test

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -6,19 +6,26 @@ class ReviewTest < ActiveSupport::TestCase
   end
 
   test "Rating must be present to be valid review and must correctly call attributes" do
+    #correctly calls attributes of valid Review object
     assert reviews(:best).valid?
     assert_equal reviews(:best).rating, 5
     assert_equal reviews(:best).feedback, "Awesome stuff :)"
     assert_equal reviews(:best).product, products(:goods)
 
+    #no rating makes the object invalid
     reviews(:best).rating = nil
     assert_not reviews(:best).valid?
   end
 
   test "Rating must be an integer between 1 and 5." do
+    #Average Review object is initially valid
     assert reviews(:average).valid?
+    #If the rating is not an integer, the object is invalid.
     reviews(:average).rating = "one"
     assert_not reviews(:average).valid?
+    reviews(:average).rating = 2.5
+    assert_not reviews(:average).valid?
+    #If the rating is less than 0 or greater than 5,
     reviews(:average).rating = 0
     assert_not reviews(:average).valid?
     reviews(:average).rating = 6
@@ -26,7 +33,10 @@ class ReviewTest < ActiveSupport::TestCase
   end
 
   test "Review must belong to a product" do
+    #Worst Review object is initially valid and belongs to "poo" product.
     assert reviews(:worst).valid?
+    assert_equal reviews(:worst).product, products(:poo)
+    #If the review is not assigned to a product, the object is invalid.
     reviews(:worst).product_id = nil
     assert_not reviews(:worst).valid?
   end
