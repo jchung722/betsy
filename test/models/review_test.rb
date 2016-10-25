6d361b2550a71f7f0d50990aaf9df5e4d5844dbb
Jessica <jchung722@gmail.com>
2016-10-25 14:05:32 -0700

    refactoring review test

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -5,19 +5,30 @@ class ReviewTest < ActiveSupport::TestCase
     assert true
   end
 
-  test "Rating must be present to be valid review." do
+  test "Rating must be present to be valid review and must correctly call attributes" do
     assert reviews(:best).valid?
-    assert_not reviews(:no_rating).valid?
+    assert_equal reviews(:best).rating, 5
+    assert_equal reviews(:best).feedback, "Awesome stuff :)"
+    assert_equal reviews(:best).product, products(:goods)
+
+    reviews(:best).rating = nil
+    assert_not reviews(:best).valid?
   end
 
   test "Rating must be an integer between 1 and 5." do
-    assert_not reviews(:no_integer).valid?
-    assert_not reviews(:low_rating).valid?
-    assert_not reviews(:high_rating).valid?
+    assert reviews(:average).valid?
+    reviews(:average).rating = "one"
+    assert_not reviews(:average).valid?
+    reviews(:average).rating = 0
+    assert_not reviews(:average).valid?
+    reviews(:average).rating = 6
+    assert_not reviews(:average).valid?
   end
 
   test "Review must belong to a product" do
-    assert_not reviews(:no_product).valid?
+    assert reviews(:worst).valid?
+    reviews(:worst).product_id = nil
+    assert_not reviews(:worst).valid?
   end
 
 end
