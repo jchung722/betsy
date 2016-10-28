6d1e5586eb2ed5a4cb67afa71b7e6a72e319bc61
Jessica <jchung722@gmail.com>
2016-10-28 10:22:49 -0700

    added custom method tests to merchant model test

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -21,4 +21,16 @@ test "merchants with minimal information must be valid" do
     assert_equal false, merchant.valid?
   end
 
+  test "Orders method should return a filtered array of orders belonging to merchant given allowable array of orders" do
+    #:testorder1 and :goodorder are related to :bob merchant; method should only allow :goodorder to be listed in array.
+    assert merchants(:bob).orders([orders(:goodorder)]), [orders(:goodorder)]
+    assert_not merchants(:bob).orders([orders(:goodorder)]).include?(orders(:testorder1))
+  end
+
+  test "order_by_status method should return a filtered array of orders belonging to merchant given a status to filter for" do
+    #:testorder1, :testorder2, and :pendingorder are related to :jane merchant; method should only allow :pendingorder to be listed in array.
+    assert merchants(:jane).order_by_status("pending"), [orders(:pendingorder)]
+    assert_not merchants(:jane).order_by_status("pending").include?(orders(:testorder1))
+  end
+
 end
