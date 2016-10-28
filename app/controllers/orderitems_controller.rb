b36754f415aeae99eaca37991dc49a614efb07c6
Jessica <jchung722@gmail.com>
2016-10-28 01:37:38 -0700

    shipped button

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -76,9 +76,9 @@ class OrderitemsController < ApplicationController
     redirect_to carts_index_path
   end
 
-  def fulfill
+  def ship
     @orderitem = Orderitem.find(params[:id])
-    @orderitem.status = "Fulfilled"
+    @orderitem.status = "Shipped"
     @orderitem.save
 
     redirect_to orders_show_path(@orderitem.order)
