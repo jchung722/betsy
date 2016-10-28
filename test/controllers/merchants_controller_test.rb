b24e6fc90b755a9f67e54b311118371cd4cdf202
Melissa Rodriguez <olenellina@gmail.com>
2016-10-28 09:11:46 -0700

    tests for merchants controller

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -2,17 +2,11 @@ require 'test_helper'
 
 class MerchantsControllerTest < ActionController::TestCase
 
-  test "should get index only for logged in merchants" do
-    get :index
-    assert_response :redirect
-    assert_equal flash[:notice], "You must be logged in to access merchant section"
-  end
-
   test "update operations for users not logged in as merchants will redirect" do
     put :update, :id => merchants(:merchant1), :merchant => {:displayname => "New Name"}
     assert_response :redirect
     assert_redirected_to homepages_index_path
   end
 
-  
+
 end
